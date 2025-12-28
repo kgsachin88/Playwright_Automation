3320d8e077538719a79101718790a4141557bc0e
kgsac <kgsachin.88@gmail.com>
2025-12-28 10:50:51 +0530

    updated code

--- tests/Actions_DD_CB_RB.spec.js
+++ tests/Actions_DD_CB_RB.spec.js
@@ -4,12 +4,21 @@ test('Dropdown and Checkbox Test',async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = 'rahulshettyacademy';
     const passWord = 'learning';
+    const optionToSelect = 'consult';
+    const radioButton = page.locator('.radiotextsty');
     await page.locator('#username').fill(userName);
     await page.locator('#password').fill(passWord);
-    await page.locator('select.form-control').selectOption('consult');
+    // To select from the simple dropdown
+    await page.locator('select.form-control').selectOption(optionToSelect);
     await page.locator('#terms').check();
-    await page.locator('#signInBtn').click();
-    console.log(await page.locator('.checkmark').first().isChecked());
-    expect(await page.locator('.checkmark').first().isChecked()).toBeTruthy();
- 
+    //To check the status of the first radio button
+    console.log(await radioButton.first().isChecked());
+    expect(await radioButton.first().isChecked()).toBeTruthy();
+    //To click on the second radio button
+    await radioButton.nth(1).click();
+    expect(await radioButton.nth(1).isChecked()).toBeTruthy();
+    expect(await radioButton.first().isChecked()).toBeFalsy();
+    await page.locator('#okayBtn').click();
+    //to puse the execution we use await page.pause()
+    //await page.pause();
 });
