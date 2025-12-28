d177991f33831cf60bb19cd77e7773e73b6e2ee3
kgsac <kgsachin.88@gmail.com>
2025-12-28 10:29:42 +0530

    Added one test for the to see the checkbox status

--- tests/Actions_DD_CB_RB.spec.js
+++ tests/Actions_DD_CB_RB.spec.js
@@ -1,1 +1,15 @@
 const {expect,test} = require('@playwright/test');
+
+test('Dropdown and Checkbox Test',async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = 'rahulshettyacademy';
+    const passWord = 'learning';
+    await page.locator('#username').fill(userName);
+    await page.locator('#password').fill(passWord);
+    await page.locator('select.form-control').selectOption('consult');
+    await page.locator('#terms').check();
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator('.checkmark').first().isChecked());
+    expect(await page.locator('.checkmark').first().isChecked()).toBeTruthy();
+ 
+});
