2f68874a49b3e006644740311571ac20804e3b16
kgsac <kgsachin.88@gmail.com>
2026-01-05 14:17:18 +0530

    relocation

--- tests/practice/OrangeLogin.spec.js
+++ tests/practice/OrangeLogin.spec.js
@@ -1,5 +1,5 @@
 const{expect, test} = require('@playwright/test');
-const Creds = require('../TestData/creds.json');
+const Creds = require('../../TestData/creds.json');
 
 test('OrangeHRM Login Test', async({page}) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
